669541edc2b547cfac80ac4899639a3a04ae8cd8
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-02-19 22:14:22 -0500

    Clean up API adapter mocking.

--- src/js/lib/api/index.js
+++ src/js/lib/api/index.js
@@ -1,6 +1,4 @@
 import * as mockApi from './mock';
 import * as api from './api';
 
-const mock = !!process.env.MOCK_API;
-export const login = mock ? mockApi.login : api.login;
-export const getUser = mock ? mockApi.getUser : api.getUser;
+module.exports = process.env.MOCK_API ? mockApi : api;
